baec890fea4943bb81a15da223524050d7fcd175
madScript <madscript01official@gmail.com>
2018-03-02 01:15:20 +0000

    Updating Script for a Universal Version (part 2)

--- nethunter.sh
+++ nethunter.sh
@@ -1,10 +1,10 @@
-#!/system/bin/sh
+#!/bin/bash
 
-echo -----------------------------------------
-echo ----------- Made By:madScript -----------
-echo ------- Kali NetHunter Installer --------
-echo ------------------v5.0-------------------
-echo -----------------------------------------
+echo      -----------------------------------------
+echo      ----------- Made By:madScript -----------
+echo      ------- Kali NetHunter Installer --------
+echo      ------------------v5.0-------------------
+echo      -----------------------------------------
 echo
 echo "##################################################"
 echo "##                                              ##"
@@ -19,6 +19,8 @@ echo "##  88       Y8b d8'          '8b 888888888 88  ##"
 echo "##                                              ##"
 echo "################### NetHunter ####################"
 echo
+
+# Installing Apps
 pm install /sdcard/install_nh/Apks/BlueNMEA.apk
 pm install /sdcard/install_nh/Apks/cSploit-release.apk
 pm install /sdcard/install_nh/Apks/Drivedroid.apk
@@ -32,6 +34,22 @@ pm install /sdcard/install_nh/Apks/USBKeyboard.apk
 pm install /sdcard/install_nh/Apks/VNC-nh.apk
 pm install /sdcard/install_nh/Apks/nethunter.apk
 sleep 5
+
+# Setting Variables
 mount -o rw,remount /system
 mount -o rw,remount /data
+arch="getprop ro.product.cpu.abi"
+amd64="sh Busybox/amd64/tools/installbusybox.sh"
+arm64="sh Busybox/arm64/tools/installbusybox.sh"
+arm="sh Busybox/arm/tools/installbusybox.sh"
+i386="sh Busybox/i386/tools/installbusybox.sh"
+
+# Choosing Right Busybox
 echo "Installing Nethunter Busybox..."
+if $arch 1> arm64*
+ then $arm64
+  else
+if $arch 1> arm*
+ then $arm
+  else
+if $arch 1>
